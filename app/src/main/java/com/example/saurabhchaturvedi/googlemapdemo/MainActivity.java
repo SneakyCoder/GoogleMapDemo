a10984c3e70759f1a7dbfca3f3875b6e4037a8fa
Saurabh.Chaturvedi <saurabh.chaturvedi@infogain.com>
2016-09-07 17:14:44 +0530

    added google play services location api to get last known location.

--- app/src/main/java/com/example/saurabhchaturvedi/googlemapdemo/MainActivity.java
+++ app/src/main/java/com/example/saurabhchaturvedi/googlemapdemo/MainActivity.java
@@ -1,15 +1,81 @@
 package com.example.saurabhchaturvedi.googlemapdemo;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static  final  String TAG = MainActivity.class.getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastKnownLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Hi just checking pull and merge
-        //
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApiIfAvailable(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastKnownLocation != null) {
+            //mLastKnownLocation.setText(String.valueOf(mLastKnownLocation.getLatitude()));
+            //mLastKnownLocation.setText(String.valueOf(mLastKnownLocation.getLongitude()));
+
+            Log.i(TAG, "Latitude and Longitude "+String.valueOf(mLastKnownLocation.getLatitude()+" "+String.valueOf(mLastKnownLocation.getLongitude())));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+        Log.i(TAG, "Latitude and Longitude onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
     }
 }
